e2cb35027ca3e8c5409ff09071a544b335ca16aa
Devraj326 <dev326patil@gmail.com>
2024-09-03 08:51:40 +0530

    added this pointer in the code and also added the comments in the code

--- HomeHub/src/EnergyManager/EnergyManager.java
+++ HomeHub/src/EnergyManager/EnergyManager.java
@@ -1,20 +1,25 @@
 package EnergyManager;
 
+// Class to manage energy consumption of devices
 public class EnergyManager {
-    private int totalConsumption;
+    private int totalConsumption; // Total energy consumption in watts
 
+    // Constructor to initialize the energy manager
     public EnergyManager() {
         this.totalConsumption = 0;
     }
 
+    // Method to add energy consumption
     public void addConsumption(int watts) {
         totalConsumption += watts;
     }
 
+    // Method to reset total energy consumption
     public void resetConsumption() {
         totalConsumption = 0;
     }
 
+    // Method to get the total energy consumption
     public int getTotalConsumption() {
         return totalConsumption;
     }

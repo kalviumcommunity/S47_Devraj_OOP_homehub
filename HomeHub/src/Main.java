02e51833d9376ed5caac939ef2eb399dbfc53f41
Devraj326 <dev326patil@gmail.com>
2024-09-04 12:27:51 +0530

    added the array of objects in the code

--- HomeHub/src/Main.java
+++ HomeHub/src/Main.java
@@ -5,17 +5,23 @@ import SecuritySystem.SecuritySystem;
 
 public class Main {
     public static void main(String[] args) {
+        // Create an instance of HomeHub
         HomeHub hub = new HomeHub();
 
+        // Create instances of different devices
         Lighting livingRoomLight = new Lighting("Living Room Light");
         Thermostat homeThermostat = new Thermostat("Home Thermostat");
         SecuritySystem homeSecurity = new SecuritySystem("Home Security");
 
-        hub.addDevice(livingRoomLight);
-        hub.addDevice(homeThermostat);
-        hub.addDevice(homeSecurity);
+        // Add devices to the HomeHub
+        hub.addLightingDevice(livingRoomLight);
+        hub.addThermostatDevice(homeThermostat);
+        hub.addSecurityDevice(homeSecurity);
 
+        // Control all devices
         hub.controlDevices();
+
+        // Display the total energy consumption
         hub.displayEnergyConsumption();
     }
 }
